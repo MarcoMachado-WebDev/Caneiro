3fffa108e688fb90f5814fabf6d887d7e466390f
MarcoMachado-WebDev <marcomachadowebdev@gmail.com>
2021-04-26 23:56:06 +0100

    added galeria map footter, but not responsive and not completed

--- caneiroWeb/src/app/app.component.ts
+++ caneiroWeb/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './app-media.component.css']
 })
 export class AppComponent {
   title = 'caneiroWeb';
@@ -12,4 +12,9 @@ export class AppComponent {
   open(){
     this.isOpen = !this.isOpen;
   }
+
+  goToElement(el :HTMLElement){
+    el.scrollIntoView({behavior:"smooth"});
+  }
+
 }
